67832727830a46357c358bff52e6ec1be91fbbf2
p00rna11 <s545523@nwmissouri.edu>
2021-11-03 05:53:28 -0500

    created and updated various files

--- routes/selector.js
+++ routes/selector.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'PoornaChand' });
+  res.render('selector', { title: 'selector' });
 });
 
 module.exports = router;
